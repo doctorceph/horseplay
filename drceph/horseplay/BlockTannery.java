fce7d7aee853aa8c2e06d4a26d3e1ecb1b5c68bb
doctorceph <ceph.dr@gmail.com>
2014-01-26 12:02:35 -0800

    Handled block behaviour

--- drceph/horseplay/BlockTannery.java
+++ drceph/horseplay/BlockTannery.java
@@ -13,9 +13,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Icon;
@@ -212,12 +215,54 @@ public class BlockTannery extends BlockContainer {
         return GameRegistry.findItem("drceph.horseplay", "leatherTanneryItem").itemID;
     }
 
+@Override
+public void breakBlock(World par1World, int par2, int par3, int par4,
+		int par5, int par6) {
+	dropItems(par1World, par2, par3, par4);
+	super.breakBlock(par1World, par2, par3, par4, par5, par6);
+}
+    
 	@Override
 	public TileEntity createNewTileEntity(World world) {
 		// TODO Auto-generated method stub
 		return new TileEntityTannery();
 	}
 	
+	
+	private void dropItems(World world, int x, int y, int z){
+        Random rand = new Random();
+
+        TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+        if (!(tileEntity instanceof IInventory)) {
+                return;
+        }
+        IInventory inventory = (IInventory) tileEntity;
+
+        for (int i = 0; i < inventory.getSizeInventory(); i++) {
+                ItemStack item = inventory.getStackInSlot(i);
+
+                if (item != null && item.stackSize > 0) {
+                        float rx = rand.nextFloat() * 0.8F + 0.1F;
+                        float ry = rand.nextFloat() * 0.8F + 0.1F;
+                        float rz = rand.nextFloat() * 0.8F + 0.1F;
+
+                        ItemStack newItem = new ItemStack(item.itemID, item.stackSize, item.getItemDamage());
+                        if (item.hasTagCompound()) {
+                            newItem.setTagCompound((NBTTagCompound) item.getTagCompound().copy());
+                        }
+                        EntityItem entityItem = new EntityItem(world,
+                                        x + rx, y + ry, z + rz,
+                                        newItem);
+
+                        float factor = 0.05F;
+                        entityItem.motionX = rand.nextGaussian() * factor;
+                        entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
+                        entityItem.motionZ = rand.nextGaussian() * factor;
+                        world.spawnEntityInWorld(entityItem);
+                        item.stackSize = 0;
+                }
+        }
+}
 	
     
 }
