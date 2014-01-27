7dea849b5f845b568ad9e55dc98d7ee321442a5f
doctorceph <ceph.dr@gmail.com>
2014-01-27 12:22:21 -0800

    Change profiler stat binning from five to seven bins (closes #6)

--- drceph/horseplay/ItemHorseProfiler.java
+++ drceph/horseplay/ItemHorseProfiler.java
@@ -23,29 +23,35 @@ public class ItemHorseProfiler extends Item {
 	}
 	
 	private static String getHealthBin (double health) {
-		if (health < 18.0) return "Low";
-		if (health >= 18.0 && health < 21.0) return "Medium-Low";
-		if (health >= 21.0 && health < 24.0) return "Medium";
-		if (health >= 24.0 && health < 27.0) return "Medium-High";
-		if (health >= 27.0) return "High";
+		if (health < 17.14) return "Lowest";
+		if (health >= 17.14 && health < 19.28) return "Lower";
+		if (health >= 19.28 && health < 21.42) return "Low";
+		if (health >= 21.42 && health < 23.56) return "Normal";
+		if (health >= 23.56 && health < 25.7) return "High";
+		if (health >= 25.7 && health < 27.84) return "Higher";
+		if (health >= 27.84) return "Highest";
 		return "Error";
 	}
 	
 	private static String getJumpBin (double jump) {
-		if (jump < 0.52) return "Low";
-		if (jump >= 0.52 && jump < 0.64) return "Medium-Low";
-		if (jump >= 0.64 && jump < 0.76) return "Medium";
-		if (jump >= 0.76 && jump < 0.88) return "Medium-High";
-		if (jump >= 0.88) return "High";
+		if (jump < 0.4857) return "Lowest";
+		if (jump >= 0.4857 && jump < 0.5714) return "Lower";
+		if (jump >= 0.5714 && jump < 0.6571) return "Low";
+		if (jump >= 0.6571 && jump < 0.7428) return "Normal";
+		if (jump >= 0.7428 && jump < 0.8285) return "High";
+		if (jump >= 0.8285 && jump < 0.9142) return "Higher";
+		if (jump >= 0.9142) return "Highest";
 		return "Error";
 	}
 	
 	private static String getSpeedBin (double speed) {
-		if (speed < 0.1575) return "Slow";
-		if (speed >= 0.1575 && speed < 0.2025) return "Medium-Slow";
-		if (speed >= 0.2025 && speed < 0.2475) return "Medium";
-		if (speed >= 0.2475 && speed < 0.2925) return "Medium-Fast";
-		if (speed >= 0.2925) return "Fast";
+		if (speed < 0.14464) return "Slowest";
+		if (speed >= 0.14464 && speed < 0.17678) return "Slower";
+		if (speed >= 0.17678 && speed < 0.20892) return "Slow";
+		if (speed >= 0.20892 && speed < 0.24106) return "Normal";
+		if (speed >= 0.24106 && speed < 0.2732) return "Fast";
+		if (speed >= 0.2732 && speed < 0.30534) return "Faster";
+		if (speed >= 0.30534) return "Fastest";
 		return "Error";
 	}
 
